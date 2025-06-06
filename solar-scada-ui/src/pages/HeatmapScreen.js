fdb6238f58771c732dae153df2494f98c76dc2d4
subramaniksn <subramani.ksn@gmail.com>
2025-06-06 17:28:01 +0530

    Heatmap.js

--- solar-scada-ui/src/pages/HeatmapScreen.js
+++ solar-scada-ui/src/pages/HeatmapScreen.js
@@ -2,13 +2,17 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import Heatmap from "../components/Heatmap";
 
+  const API_BASE_URL =
+    window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1"
+      ? "http://localhost:5000"
+      : "http://103.102.234.177:5000";
 const HeatmapScreen = () => {
   const [inverterData, setInverterData] = useState([]);
 
   useEffect(() => {
     const fetchInverterData = async () => {
       try {
-        const response = await axios.get("http://localhost:5000/api/inverter/Heatmap");
+        const response = await axios.get(`${API_BASE_URL}/api/inverter/Heatmap`);
         setInverterData(response.data);
       } catch (error) {
         console.error("Error fetching inverter data:", error);
